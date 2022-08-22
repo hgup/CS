805cb103ea3f648b149e65649674819397a16b33
Hursh Gupta <hurshgupta1@gmail.com>
2022-08-23 05:07:11 +0530

    believer

--- R files/believe.R
+++ R files/believe.R
@@ -24,10 +24,10 @@ x <- barplot( Percent, names.arg = F, horiz = T, border = NA,
               xlim = c(0,100), col = "grey", cex.names = 0.85, axes = F)
 
 # Label Chart
-
+POI <- c("USA", "Brazil","India")
 for (i in 1: length(Country))
 {
-  if (Country[i] %in% c("Germany", "Brasil"))
+  if (Country[i] %in% POI)
   {myFont <- "Inter Black"} else {myFont <- "Inter Light"}
   text(-8, x[i], Country[i], xpd = T, adj = 1, cex = 0.85, family= myFont)
   text(-3.5, x[i], Percent[i], xpd = T, adj = 1, cex = 0.85, family= myFont)
@@ -35,14 +35,18 @@ for (i in 1: length(Country))
 
 # Other Elements
 
-rect(0,-0.5,20,28, col = rgb(191,239,255,80, maxColorValue = 255), border = NA)
-rect(0,-0.5,40,28, col = rgb(191,239,255,120, maxColorValue = 255), border = NA)
-rect(0,-0.5,60,28, col = rgb(191,239,255,80, maxColorValue = 255), border = NA)
-rect(0,-0.5,80,28, col = rgb(191,239,255,120, maxColorValue = 255), border = NA)
-rect(0,-0.5,100,28, col = rgb(191,239,255,80, maxColorValue = 255), border = NA)
+rect(0,-0.5,20,28, col = rgb(249,219,189,80, maxColorValue = 255), border = NA)
+rect(20,-0.5,40,28, col = rgb(249,219,189,120, maxColorValue = 255), border = NA)
+rect(40,-0.5,60,28, col = rgb(249,219,189,80, maxColorValue = 255), border = NA)
+rect(60,-0.5,80,28, col = rgb(249,219,189,120, maxColorValue = 255), border = NA)
+rect(80,-0.5,100,28, col = rgb(249,219,189,80, maxColorValue = 255), border = NA)
 
-myValue2 <- c(rep(0,4),27,rep(0,8),84,0,0)
-myColour2 <- rgb(255,0,210, maxColorValue = 255)
+myValue2 <- c()
+for (i in 1: length(Country)){
+  if (Country[i] %in% POI)
+  {myValue2 <- c(myValue2,Percent[i])} else {myValue2 <- c(myValue2,0)}
+}
+myColour2 <- rgb(3,29,68, maxColorValue = 255)
 x2 <- barplot(myValue2, name.arg = F, horiz = T, border = NA, xlim = c(0,100),
               col = myColour2, cex.names = 0.85, axes = F, add = T)
 
@@ -59,7 +63,7 @@ mtext(c(0,20,40,60,80,100), at = c(0,20,40,60,80,100), 1, line = 0, cex = 0.80)
 
 mtext("`I definitely Believe in God or a Supreme Being`", 3, line = 1.3,
       adj = 0, cex = 1.2, family = "Inter Black", outer = T)
-mtext("was said in 2010 in :", 3, line = -0.4, adj = 0.9, outer = T)
+mtext("was said in 2010 in :", 3, line = -0.4, adj = 0, outer = T)
 mtext("Source: www.ipsos-na.com, Desigh: Stefan Fichtel, ixtract", 1, line =1,
        adj = 1.0, cex = 0.65, outer = T, font = 3)
 
---
+++ docs/rwisdom.md
@@ -0,0 +1,3 @@
+# dev.off()
+
+Sometimes plots don't show up in rstudio's default plot plane. In order to get them there, we must first turn **off** the existing devices by using `dev.off()` till there is only a `null device` left.
