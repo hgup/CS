2eddf4dd87836fa55b0a6da02d388ddb9f674f7f
Hursh Gupta <hurshgupta1@gmail.com>
2022-09-09 10:16:43 +0530

    Final Colors

--- assignments/UCSC302_SE1_2022/colors.R
+++ assignments/UCSC302_SE1_2022/colors.R
@@ -1,5 +1,15 @@
-myC1<-rgb(0,208, 226, maxColorValue=255)
-myC2<-rgb(109,221, 225, maxColorValue=255)
-myC3<-rgb(255,138, 238, maxColorValue=255)
-myC4<-rgb(255,0, 210, maxColorValue=255 )
+#myC1<-rgb(0,208, 226, maxColorValue=255)
+#myC2<-rgb(109,221, 225, maxColorValue=255)
+#myC3<-rgb(255,138, 238, maxColorValue=255)
+#myC4<-rgb(255,0, 210, maxColorValue=255 )
+
+#myC1<-rgb(92,171, 125, maxColorValue=255)
+#myC2<-rgb(147,200, 169, maxColorValue=255 )
+myC4<-rgb(77,157, 224, maxColorValue=255)
+myC3<-rgb(116,179, 231, maxColorValue=255)
+
+myC2<- "#5bba6f"
+myC1<- "#3fa34d"
+#myC3<- "#4B88A2"
+#myC4<- "#F9F4F5"
 colours<-c("grey",myC1, myC2,myC3,myC4)
