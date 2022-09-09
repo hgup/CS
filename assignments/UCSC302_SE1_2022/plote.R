9273d81f2b5e42d6ae0c8bd75831d677b676a4ca
Hursh Gupta <hurshgupta1@gmail.com>
2022-09-09 10:16:27 +0530

    Final Working

--- assignments/UCSC302_SE1_2022/plote.R
+++ assignments/UCSC302_SE1_2022/plote.R
@@ -1,16 +1,18 @@
 # Open a Pdf device: output to pdf/views.pdf
 pdf_file <- "pdf/views.pdf"
-cairo_pdf(pdf_file, bg = "grey98", width = 9, height = 6.5)
+cairo_pdf(pdf_file, bg = "#191919", width = 13, height = 10.5)
 
 
 # Some default parameters
-  # outer margins: 0.65, 0.25, 0.75, 0.75
-  # margin size: 0.3,2,0.35,0
+  # outer margins: 0.25,0.75,1,0.75
+  # margin size: 1.8,3.75,0.25,0
   # font: Inter Light
   # text orientation: horizontal
-par(omi = c(0.5,2,0.6,0),
-    mai = c(0.5,0.5,0.5,0.5),
+par(omi = c(0.25,0.75,1.5,0.75),
+    mai = c(1.8,3.75,0.25,0),
     family = "Inter Light",
+    lheight = 1.5,
+    col = "white",
     las = 1)
 
 
@@ -41,7 +43,7 @@ source("colors.r",encoding="UTF-8" )
 
 # plot the NOT SURE
 x <- barplot(-rep(d_max, 6), cex.names=1.1,horiz=T,
-        border=par ("bg"), xlim=c(-d_max,d_max), col=colours[1],axes=T)
+        border=par ("bg"), xlim=c(-d_max,d_max), col=colours[1],axes=F)
         
 barplot (-(d_max-data[1, ]), horiz =T,
         border=par("bg"),col=par("bg"),axes=F, add=T)
@@ -60,21 +62,25 @@ myfont = "Inter Light"
 
 # Labels
 for (i in 1: length(as.matrix(views_data[,1]))){
-  text(-155, x[i],paste(strwrap(views_data[i,1],width = 40),collapse = '\n'), xpd = NA, adj = 1, cex = 0.65, family = myfont)
+  text(-155, x[i], paste(strwrap(views_data[i,1],width = 50),collapse = '\n'), xpd = NA, adj = 1, cex = 1.1, family = myfont)
   
 }
 
 # Arrows and stuff
+myresponses <- c("Not Sure","Agree Strongly", "Agree", "Disagree", "Disagree Strongly")
 
 arrows(0,-0.1,0,8.6,lwd=2.5, length=0, xpd=T, col="Skyblue3")
-px<-c(-98, -87,-41,15,65); tx<-c(-105, -60,-26,8,60); y<-rep(-1,5)
+px<-c(-160, -140,-40,40,120); tx<-c(-165, -85,-45,35,115)-2; y<-rep(-1,5)
 points(px, y, pch=15, cex=4, col=colours,xpd=T )
-mtext (c(80, 60, 40, 20,0, 20, 40, 60) , at=c(-80, -60, -40, -20,0, 20, 40,60), line=0, cex=0.95)
+mtext (c(120,100, 80, 60, 40, 20,0, 20, 40, 60, 80, 100,120) , at=c(-120,-100, -80, -60, -40, -20,0, 20, 40, 60, 80, 100, 120),1, line=1, cex=0.95)
+text (tx, y,myresponses,adj=1,xpd=T, font=3, cex=1)
+
 
 # Titling
-mtext("Opinion about Long Weekends",3, line=2.2, adj=0, cex=1.8, outer=T, family="Inter Black")
+mtext("Opinion about Long Weekends",3, line=2.2, adj=0, cex=2.5, outer=T, family="Inter Black")
+mtext("Agree Vs Disagree",3, line=0, adj=0, cex=2.5/1.5, outer=T,font = 4, family="Inter Thin")
 mtext( "Values in Numbers",3,line=1, adj=1, cex=0.95, font=3)
-mtext("UCSC-301 Assignment",1,line=5.2,adj =1, cex=0.95, font=3)
+mtext("UCSC-301 Assignment. Code available @ https://github.com/hgup/cs/tree/main/assignments/UCSC302_SE1_2022",1,line=7,adj =1, cex=0.95, font=3)
 mtext ("N=150",3, line=1, adj=0, cex=1.15, family="Inter", font=3)
 
 
